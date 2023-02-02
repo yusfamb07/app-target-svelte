de34b1f5c3ff7c767a09e45a82221646d884f3ee
endabelyu <endabelyuproject@gmail.com>
2023-02-02 16:38:46 +0700

    change a few label in analyst dashboard, but the char still the same with lawfull (need a solution to pass value from child to component chart

--- src/routes/analyst/dashboard/+page.svelte
+++ src/routes/analyst/dashboard/+page.svelte
@@ -8,6 +8,7 @@
 	});
 </script>
 
+<!-- pake apex chart -->
 <div class="konten">
 	<div class="container-fluid mt-3">
 		<div class="row">
@@ -15,17 +16,6 @@
 				<div class="card">
 					<div class="card-body">
 						<h6>Lawful Interception Request</h6>
-						<div class="d-flex align-items-center gap-5 mt-3">
-							<img class="icon-dashboard" src="/lawfull.svg" alt="" />
-							<h1 id="total-lawful" class="total-lawful">56</h1>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="card">
-					<div class="card-body">
-						<h6>Geolocation Request</h6>
 						<div class="d-flex align-items-center gap-5 mt-3">
 							<img class="icon-dashboard" src="/geo.svg" alt="" />
 							<h1 id="total-lawful" class="total-lawful">56</h1>
@@ -36,10 +26,10 @@
 			<div class="col-md-3">
 				<div class="card">
 					<div class="card-body">
-						<h6>Telecomunication Request</h6>
+						<h6>Geolocation Request</h6>
 						<div class="d-flex align-items-center gap-5 mt-3">
-							<img class="icon-dashboard" src="/tele.svg" alt="" />
-							<h1 id="total-lawful" class="total-lawful">56</h1>
+							<img class="icon-dashboard" src="/lawfull.svg" alt="" />
+							<h1 id="total-lawful" class="total-lawful">22</h1>
 						</div>
 					</div>
 				</div>
@@ -47,7 +37,18 @@
 			<div class="col-md-3">
 				<div class="card">
 					<div class="card-body">
-						<h6>Pending Request</h6>
+						<h6>Telecomunication Request</h6>
+						<div class="d-flex align-items-center gap-5 mt-3">
+							<img class="icon-dashboard" src="/tele.svg" alt="" />
+							<h1 id="total-lawful" class="total-lawful">121</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="card">
+					<div class="card-body">
+						<h6>Lawful Interception Request</h6>
 						<div class="d-flex align-items-center gap-5 mt-3">
 							<img class="icon-dashboard" src="/req.svg" alt="" />
 							<h1 id="total-lawful" class="total-lawful">56</h1>
@@ -74,7 +75,7 @@
 					<div class="card-body">
 						<div class="d-flex justify-content-between">
 							<h6>Most Request by Binda</h6>
-							<h6>Lawful Interception Request <img src="/down.png" alt="" /></h6>
+							<h6>Binda Jakarta <img src="/down.png" alt="" /></h6>
 						</div>
 						<div class="mt-3">
 							<BarChart />
@@ -90,8 +91,10 @@
 					<div class="card-body">
 						<div class="d-flex justify-content-between mb-3">
 							<h6>Request List</h6>
-							<div class="d-flex">
-								<form action="">
+							<div class="d-flex ">
+								<form action="" class="date-container">
+									<input type="date" name="" id="" />
+									<p>To</p>
 									<input type="date" name="" id="" />
 								</form>
 							</div>
@@ -109,60 +112,60 @@
 							</thead>
 							<tbody>
 								<tr>
-									<td>South Korea</td>
-									<td>10</td>
-									<td>20</td>
-									<td>61</td>
-									<td>25</td>
-									<td>25</td>
+									<td>1</td>
+									<td>Binda DKI Jakarta</td>
+									<td>234</td>
+									<td>123</td>
+									<td>124</td>
+									<td>12</td>
 								</tr>
 								<tr>
-									<td>France</td>
-									<td>10</td>
-									<td>20</td>
-									<td>61</td>
-									<td>25</td>
-									<td>25</td>
+									<td>1</td>
+									<td>Binda DKI Jakarta</td>
+									<td>234</td>
+									<td>123</td>
+									<td>124</td>
+									<td>12</td>
 								</tr>
 								<tr>
-									<td>Japan</td>
-									<td>10</td>
-									<td>20</td>
-									<td>61</td>
-									<td>25</td>
-									<td>25</td>
+									<td>1</td>
+									<td>Binda DKI Jakarta</td>
+									<td>234</td>
+									<td>123</td>
+									<td>124</td>
+									<td>12</td>
 								</tr>
 								<tr>
-									<td>Italy</td>
-									<td>10</td>
-									<td>20</td>
-									<td>61</td>
-									<td>25</td>
-									<td>25</td>
+									<td>1</td>
+									<td>Binda DKI Jakarta</td>
+									<td>234</td>
+									<td>123</td>
+									<td>124</td>
+									<td>12</td>
 								</tr>
 								<tr>
-									<td>Netherlands</td>
-									<td>10</td>
-									<td>20</td>
-									<td>61</td>
-									<td>25</td>
-									<td>25</td>
+									<td>1</td>
+									<td>Binda DKI Jakarta</td>
+									<td>234</td>
+									<td>123</td>
+									<td>124</td>
+									<td>12</td>
 								</tr>
 								<tr>
-									<td>Canada</td>
-									<td>10</td>
-									<td>20</td>
-									<td>61</td>
-									<td>25</td>
-									<td>25</td>
+									<td>1</td>
+									<td>Binda DKI Jakarta</td>
+									<td>234</td>
+									<td>123</td>
+									<td>124</td>
+									<td>12</td>
 								</tr>
 								<tr>
-									<td>United Kingdom</td>
-									<td>10</td>
-									<td>20</td>
-									<td>61</td>
-									<td>25</td>
-									<td>25</td>
+									<td>1</td>
+									<td>Binda DKI Jakarta</td>
+									<td>234</td>
+									<td>123</td>
+									<td>124</td>
+									<td>12</td>
 								</tr>
 							</tbody>
 						</table>
@@ -172,3 +175,17 @@
 		</div>
 	</div>
 </div>
+
+<style>
+	.date-container {
+		align-items: baseline;
+		display: flex;
+		gap: 1rem;
+	}
+	input[type='date'] {
+		background: #23282e;
+		border-right: none;
+		border-left: none;
+		color: #fbfdff;
+	}
+</style>
